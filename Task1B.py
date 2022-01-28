f09a6fe88a73a789af68df3fa4c50ecfbf4edb16
Sunaabh Trivedi <tsunaabh@gmail.com>
2022-01-28 20:32:58 +0000

    Updated Task1B.py with closest/furthest 10

--- Task1B.py
+++ Task1B.py
@@ -3,6 +3,15 @@ from floodsystem.geo import stations_by_distance
 
 stations = build_station_list()
 
-p = (48.8567, 2.3508)
+#Co-ordinates of Cambridge city centre
+p = (52.2053, 0.1218)
 
-print(stations_by_distance(stations, p))  
+x = stations_by_distance(stations, p)
+
+#Formatting result into form (city name, town, distance)
+formatted_x = []
+for i in x:
+    formatted_x.append((i[0].name, i[0].town, i[1]))
+
+print(f"Closest 10: {formatted_x[:10]}")
+print(f"Furthest 10: {formatted_x[-10:]}")
